c02620e48172cc8120e7b4ac2241dfb2417f0aba
Navoditn <39704421+navodit15@users.noreply.github.com>
2020-02-26 20:44:50 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -9,13 +9,27 @@ using namespace std;
 #define pb push_back
 #define fst first
 #define snd second
-typedef long long ll;typedef pair<int, int> pii;typedef vector<int> vi;
 template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);}
 template<typename T> T lcm(T a, T b){return(a*(b/gcd(a,b)));}
+typedef long long ll;typedef pair<int, int> pii;typedef vector<int> vi;typedef long double ld;
 template<class T> ostream& operator<<(ostream &os, vector<T> V) {os << "[ "; for(auto v : V) os << v << " "; return os << "]";}
 template<class T> ostream& operator<<(ostream &os, set<T> S){os << "{ "; for(auto s:S) os<<s<<" "; return os<<"}";}
 template<class L, class R> ostream& operator<<(ostream &os, pair<L,R> P) {return os << "(" << P.first << "," << P.second << ")";}
 template<class L, class R> ostream& operator<<(ostream &os, map<L,R> M) {os << "{ "; for(auto m:M) os<<"("<<m.first<<":"<<m.second<<") "; return os<<"}";}
+namespace fio{
+	template <typename T> void read(T &x) { x = 0; int f = 1;char c = getchar_unlocked();
+		for (; !isdigit(c); c = getchar_unlocked()) if (c == '-') f = -f;
+		for (; isdigit(c); c = getchar_unlocked()) x = x * 10 + c - '0';
+		x *= f;
+	}
+	template<class T> void read(vector<T>& a);
+	template<class T> void read(vector<T>& a) { rep(i,0,sz(a)) read(a[i]); }
+	template<class T, class... Ts> void read(T& t, Ts&... ts) { read(t); read(ts...);}
+	template <typename T> void write(T x) { if (x < 0){ x = -x; putchar('-');}if (x > 9) {write(x / 10);}putchar(x % 10 + '0');}
+	template <typename T> void writeln(T x) { write(x);puts("");}
+	template<class T, class... Ts> void write(const T& t, const Ts&... ts) { write(t); write(ts...); }
+}
+using namespace fio; 
 
 //#include <ext/pb_ds/assoc_container.hpp>
 //#include <ext/pb_ds/tree_policy.hpp> 
@@ -35,12 +49,13 @@ template<typename Arg1, typename ... Args>void __f(const char *names, Arg1 &&arg
 #endif
 
 
+
+
 int main() {
     cin.sync_with_stdio(0); cin.tie(0);
     cin.exceptions(cin.failbit);
 	
 	
- 
 
 #ifdef np
     cerr <<endl<<endl<< "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
