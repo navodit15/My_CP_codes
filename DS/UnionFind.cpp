a10a94f05dc924177a7cd5a25bee7fd1fe416cf6
Navoditn <39704421+navodit15@users.noreply.github.com>
2020-04-02 21:10:12 +0530

    Update UnionFind.cpp

--- DS/UnionFind.cpp
+++ DS/UnionFind.cpp
@@ -1,32 +1,18 @@
-#define N 100010
-int parent[N];	//immediate parent of every node
-int Rank[N];	//Rank of each subset, where a subset is identified by its root.
-
-int find(int cur)
-{
-	if(parent[cur]==cur)
-		return cur;
-	return parent[cur]=find(parent[cur]);
-}
-
-void Union(int x,int y)
-{
-	int xroot=find(x);
-	int yroot=find(y);
-	if(xroot==yroot)
-		return;
-	if(Rank[xroot]<Rank[yroot])
-		swap(xroot,yroot);
-	parent[yroot]=xroot;
-	if(Rank[xroot]==Rank[yroot])
-		Rank[xroot]++;
-}
-void init(int n)
-{
-	int i;
-	for(i=0;i<=n;++i)
-	{
-		Rank[i]=0;
-		parent[i]=i;
+//ref~ https://github.com/bqi343/USACO/blob/master/Implementations/content/graphs%20(12)/DSU/DSU%20(7.6).h
+struct DSU {
+	vi e; void init(int n) { e = vi(n,-1); }
+	int find(int x) { return e[x] < 0 ? x : e[x] = find(e[x]); } 
+	bool sameSet(int a, int b) { return find(a) == find(b); }
+	int size(int x) { return -e[find(x)]; }
+	bool unite(int x, int y) { // union-by-rank
+		x = find(x), y = find(y); if (x == y) return 0;
+		if (e[x] > e[y]) swap(x,y);
+		e[x] += e[y]; e[y] = x; return 1;
 	}
-}
+};
+/**template<class T> T kruskal(int n, vector<pair<T,pii>> ed) {
+	sort(all(ed));
+	T ans = 0; DSU D; D.init(n+1); // edges that unite are in MST
+	trav(a,ed) if (D.unite(a.s.f,a.s.s)) ans += a.f; 
+	return ans;
+}*/
