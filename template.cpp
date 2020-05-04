fe5645b4c27dab05964c3d0c599a81d74dac4267
Navoditn <39704421+navodit15@users.noreply.github.com>
2020-05-04 06:44:41 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+#pragma GCC optimize("O3")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize("unroll-loops")
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp> 
+#define int long long
 #define fr(i,a,b) for(int i = a ; i <= b ; ++i)
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define trav(a, x) for(auto& a : x)
@@ -10,8 +15,6 @@ using namespace std;
 #define pb push_back
 #define fst first
 #define snd second
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp> 
 using namespace __gnu_pbds;
 using ordered_set = tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>;  
 template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);}
@@ -21,6 +24,15 @@ template<class T> ostream& operator<<(ostream &os, vector<T> V) {os << "[ "; for
 template<class T> ostream& operator<<(ostream &os, set<T> S){os << "{ "; for(auto s:S) os<<s<<" "; return os<<"}";}
 template<class L, class R> ostream& operator<<(ostream &os, pair<L,R> P) {return os << "(" << P.first << "," << P.second << ")";}
 template<class L, class R> ostream& operator<<(ostream &os, map<L,R> M) {os << "{ "; for(auto m:M) os<<"("<<m.first<<":"<<m.second<<") "; return os<<"}";}
+namespace tricks{
+  template<typename T> void remdup(vector<T>& v) {sort(all(v)); v.erase(unique(all(v)), v.end());}
+    template<typename T> typename vector<T>::iterator find(vector<T>& v, const T& x) {
+        auto it = lower_bound(all(v), x); return it != v.end() && *it == x ? it : v.end();
+    }
+    template<typename T> size_t index(vector<T>& v, const T& x) { //0-based
+        auto it = find(v, x); assert(it != v.end() && *it == x); return it - v.begin();
+    }
+} using namespace tricks;
    
 #ifdef np
 #define trace(...) cout<<"Line:"<<__LINE__<<" "; __f(#__VA_ARGS__, __VA_ARGS__)
@@ -34,10 +46,30 @@ template<typename Arg1, typename ... Args>void __f(const char *names, Arg1 &&arg
 #define endl '\n'
 #endif
 
+#define MOD 1000000007  
+inline int add(int x, int y){ x += y; if(x >= MOD) x -= MOD; return x;}
+inline int sub(int x, int y){ x -= y; if(x < 0) x += MOD; return x;}
+inline int mul(int x, int y){ return (((ll) x) * y) % MOD;}
+inline ll powr(ll a , ll b){
+	ll t =1 ;
+	while(b){
+		if(b&1) t=mul(t,a);	
+		a=mul(a,a);
+		b>>=1;
+	}
+	return t%MOD;
+}
+inline ll inv(ll a){ return powr(a,MOD-2); }
+
+
+
+
+
+
 
 
 void solve(){
-
+	
 }
 
 
