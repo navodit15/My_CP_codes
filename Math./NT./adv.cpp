547f88afc31c3e486d4eca9d18dd46e70f0ebad0
Navoditn <39704421+navodit15@users.noreply.github.com>
2020-05-11 00:14:38 +0530

    Update adv.cpp

--- Math./NT./adv.cpp
+++ Math./NT./adv.cpp
@@ -76,22 +76,22 @@ template<typename Arg1, typename ... Args>void __f(const char *names, Arg1 &&arg
     //};
 //}
 const int maxn = 1e7+1;
-int factors[maxn],mu[maxn],phi[maxn];
+int factor[maxn],mu[maxn],phi[maxn];
 vi divs[maxn];
 void Mu(){
-	memset(factors,0,sizeof factors);
+	memset(factor,0,sizeof factor);
 	for(int i = 2;i*i<maxn;i++)
-		if(!factors[i])
+		if(!factor[i])
 			for(int j = i*i;j<maxn;j+=i)
-				factors[j] = i;
+				factor[j] = i;
 	mu[1] = 1;
 	for(int i = 2;i<maxn;i++)
 	{
 		int p;
-		if(!factors[i])
+		if(!factor[i])
 			p = i;
 		else
-			p = factors[i];
+			p = factor[i];
 		int j = i/p;
 		if(j%p == 0)
 			mu[i] = 0;
@@ -290,13 +290,73 @@ namespace pi{
 		return dp.back();
 	}
 }
+long long totient(long long n) {
+    long long result = n;
+ 
+    for (long long p = 2; p * p <= n; p++)
+        if (n % p == 0) {
+            result = result / p * (p - 1);
+ 
+            do {
+                n /= p;
+            } while (n % p == 0);
+        }
+ 
+    if (n > 1)
+        result = result / n * (n - 1);
+ 
+    return result;
+}
 
+
+ll modmult(ll x, ll y, ll MOD) {
+	ll a = x, r = 0;
+	while(y) {
+		if(y & 1)
+			if((r += a) >= MOD) r -= MOD;
+		if((a += a) >= MOD) a -= MOD;
+		y >>= 1;
+	}
+	return r;
+}
+ll powmodll(ll a, unsigned long long k, const ll MOD) {
+	a %= MOD;
+	ll r = MOD == 1 ? 0 : 1;
+	while(k) {
+		if(k & 1)
+			r = modmult(r, a, MOD);
+		a = modmult(a, a, MOD);
+		k >>= 1;
+	}
+	return (unsigned)r;
+}
+
+bool suspect(ll a, int s, ll d, ll n) {
+	ll x = powmodll(a, d, n);
+	if(x == 1) return true;
+	for(int r = 0; r < s; ++r) {
+		if(x == n - 1) return true;
+		x = modmult(x, x, n);
+	}
+	return false;
+}
+// {2,7,61,-1}                 is for n < 4759123141 (= 2^32)
+// {2,3,5,7,11,13,17,19,23,-1} is for n < 10^16 (at least)
+bool isPrime(ll n) {
+	if (n <= 1 || (n > 2 && n % 2 == 0)) return false;
+	int test[] = {2,3,5,7,11,13,17,19,23,-1};
+	ll d = n - 1; int s = 0;
+	while (d % 2 == 0) ++ s, d /= 2;
+	for(int i = 0; test[i] < n && test[i] != -1; ++i)
+		if(!suspect(test[i], s, d, n)) return false;
+	return true;
+}
 int main() {
     cin.sync_with_stdio(0); cin.tie(0);
     cin.exceptions(cin.failbit);
 	
 
-
+	
 
 
 
