39d7409b9525ba80e2f3c81bbc004adac5b86c15
Navoditn <39704421+navodit15@users.noreply.github.com>
2020-05-18 10:21:33 +0530

    Update fastio.cpp

--- Math./fastio.cpp
+++ Math./fastio.cpp
@@ -1,58 +1,13 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
-#define trav(a, x) for(auto& a : x)
-#define all(x) begin(x), end(x)
-#define rall(x) (x).rbegin(), (x).rend()
-#define sz(x) (int)(x).size()
-#define pb push_back
-#define fst first
-#define snd second
-template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);}
-template<typename T> T lcm(T a, T b){return(a*(b/gcd(a,b)));}
-typedef long long ll;typedef pair<int, int> pii;typedef vector<int> vi;typedef long double ld;
-template<class T> ostream& operator<<(ostream &os, vector<T> V) {os << "[ "; for(auto v : V) os << v << " "; return os << "]";}
-template<class T> ostream& operator<<(ostream &os, set<T> S){os << "{ "; for(auto s:S) os<<s<<" "; return os<<"}";}
-template<class L, class R> ostream& operator<<(ostream &os, pair<L,R> P) {return os << "(" << P.first << "," << P.second << ")";}
-template<class L, class R> ostream& operator<<(ostream &os, map<L,R> M) {os << "{ "; for(auto m:M) os<<"("<<m.first<<":"<<m.second<<") "; return os<<"}";}
-namespace fio{
-	template <typename T> void scan(T &x) { x = 0; int f = 1;char c = getchar_unlocked();
-		for (; !isdigit(c); c = getchar_unlocked()) if (c == '-') f = -f;
-		for (; isdigit(c); c = getchar_unlocked()) x = x * 10 + c - '0';
-		x *= f;
-	}
-	template<class T> void scan(vector<T>& a);
-	template<class T> void scan(vector<T>& a) { rep(i,0,sz(a)) scan(a[i]); }
-	template<class T, class... Ts> void scan(T& t, Ts&... ts) { scan(t); scan(ts...);}
-	template <typename T> void print(T x) { if (x < 0){ x = -x; putchar('-');}if (x > 9) {print(x / 10);}putchar(x % 10 + '0');}
-	template <typename T> void println(T x) { print(x);puts("");}
-	template<class T, class... Ts> void print(const T& t, const Ts&... ts) { print(t); print(ts...); }
-}
-using namespace fio; 
- 
-#ifdef np
-#define trace(...) cout<<"Line:"<<__LINE__<<" "; __f(#__VA_ARGS__, __VA_ARGS__)
-template<typename Arg1>void __f(const char *name, Arg1 &&arg1) { cout<<name<<" : "<<arg1<<endl;}
-template<typename Arg1, typename ... Args>void __f(const char *names, Arg1 &&arg1, Args &&... args) {
-    const char *comma=strchr(names+1,','); cout.write(names,comma-names)<<" : "<<arg1<<" | ";
-    __f(comma+1,args...);
-}
-#else
-#define trace(...)
-#define endl '\n'
-#endif
-
-
-
-int main() {
-    cin.sync_with_stdio(0); cin.tie(0);
-    cin.exceptions(cin.failbit);
-	
-	
-
-#ifdef np
-    cerr <<endl<<endl<< "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
-#endif  
- return 0; 
- }
+struct ios { 
+    inline char gc(){
+        static const int IN_LEN=1<<18|1;
+        static char buf[IN_LEN],*s,*t;
+        return (s==t)&&(t=(s=buf)+fread(buf,1,IN_LEN,stdin)),s==t?-1:*s++;
+    }
+    template <typename _Tp> inline ios & operator >> (_Tp&x){
+        static char ch,sgn; ch = gc(), sgn = 0;
+        for(;!isdigit(ch);ch=gc()){if(ch==-1)return *this;sgn|=ch=='-';}
+        for(x=0;isdigit(ch);ch=gc())x=x*10+(ch^'0');
+        sgn&&(x=-x); return *this;
+    }
+}CIN;
