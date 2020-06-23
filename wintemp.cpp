c6971498629e08271043a9504bbae78ef94c8344
Navoditn <39704421+navodit15@users.noreply.github.com>
2020-06-23 05:42:02 +0530

    Create wintemp.cpp

---
+++ wintemp.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("O3")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize("unroll-loops") 
+#define int long long
+#define fr(i,a,b) for(int i = a ; i <= b ; ++i)
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define fst first
+#define snd second
+#define rtime cout <<endl<<endl<< "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);}
+template<typename T> T lcm(T a, T b){return(a*(b/gcd(a,b)));}
+typedef long long ll;typedef pair<int, int> pii;typedef vector<int> vi;typedef long double ld;
+template<class T> ostream& operator<<(ostream &os, vector<T> V) {os << "[ "; for(auto v : V) os << v << " "; return os << "]";}
+template<class T> ostream& operator<<(ostream &os, set<T> S){os << "{ "; for(auto s:S) os<<s<<" "; return os<<"}";}
+template<class L, class R> ostream& operator<<(ostream &os, pair<L,R> P) {return os << "(" << P.first << "," << P.second << ")";}
+template<class L, class R> ostream& operator<<(ostream &os, map<L,R> M) {os << "{ "; for(auto m:M) os<<"("<<m.first<<":"<<m.second<<") "; return os<<"}";}
+template<typename T> void remdup(vector<T>& v) {sort(all(v)); v.erase(unique(all(v)), v.end());}
+   
+#define trace(...) cout<<"Line:"<<__LINE__<<" "; __f(#__VA_ARGS__, __VA_ARGS__)
+template<typename Arg1>void __f(const char *name, Arg1 &&arg1) { cout<<name<<" : "<<arg1<<endl;}
+template<typename Arg1, typename ... Args>void __f(const char *names, Arg1 &&arg1, Args &&... args) {
+    const char *comma=strchr(names+1,','); cout.write(names,comma-names)<<" : "<<arg1<<" | ";
+    __f(comma+1,args...);
+}
+
+
+#define MOD 1000000007  
+inline int add(int x, int y){ x += y; if(x >= MOD) x -= MOD; return x;}
+inline int sub(int x, int y){ x -= y; if(x < 0) x += MOD; return x;}
+inline int mul(int x, int y){ return (((ll) x) * y) % MOD;}
+inline ll powr(ll a , ll b){
+	ll t =1 ;
+	while(b){
+		if(b&1) t=mul(t,a);	
+		a=mul(a,a);
+		b>>=1;
+	}
+	return t%MOD;
+}
+inline ll inv(ll a){ return powr(a,MOD-2); }
+
+void setIn(string s) { freopen(s.c_str(),"r",stdin); }
+void setOut(string s) { freopen(s.c_str(),"w",stdout); }
+void unsyncIO() { ios_base::sync_with_stdio(0); cin.tie(0); }
+void setIO(string s = "") {
+	unsyncIO();
+	cin.exceptions(cin.failbit); 
+	// throws exception when do smth illegal
+	// ex. try to read letter into int
+	if (sz(s)) { setIn(s+".in"), setOut(s+".out"); } // for USACO
+}
+
+void solve(){
+
+}
+
+signed main() {
+	setIO();
+	int tt =1 ;
+	// cin >> tt;
+	rep(i,0,tt) solve();
+
+
+
+
+
+ return 0; 
+ }
