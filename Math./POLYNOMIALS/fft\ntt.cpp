f5c8beb2a1d259004c20b09c865d09e3c2406d2d
Navoditn <39704421+navodit15@users.noreply.github.com>
2020-07-11 12:38:27 +0530

    Update fft\ntt.cpp
    Add verification

--- Math./POLYNOMIALS/fft\ntt.cpp
+++ Math./POLYNOMIALS/fft\ntt.cpp
@@ -1,6 +1,8 @@
 // polymul mod
-//http://neerc.ifmo.ru/trains/toulouse/2017/fft2.pdf
+//ref : http://neerc.ifmo.ru/trains/toulouse/2017/fft2.pdf
 //fft template : https://codeforces.com/contest/993/submission/39317957
+//Verification : 
+using vll = vector<ll> ;
 struct base{
     ld x,y;
     base(){x=y=0;}
@@ -98,99 +100,23 @@ vll mul(vll a, vll b, int mod) {
     mul_big_mod(a, b, mod);
     return a;
 }
-// WITHOUT MOD
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-typedef long double Num;
-typedef complex<Num> Complex;
-double PI = 2.0*acos(0.0);
-void fft(Complex P[], int n, int oper) {
-  for (int i = 1, j = 0; i < n - 1; i++) {
-    for (int s = n; j ^= s >>= 1, ~j & s;);
-    if (i < j) swap(P[i], P[j]);
-  }
-  for (int d = 0; (1 << d) < n; d++) {
-    int m = 1 << d, m2 = m * 2;
-    Complex unit_p0(cos(-2*PI/m2*oper), sin(-2*PI/m2*oper));
-    for (int i = 0; i < n; i += m2) {
-      Complex unit(1);
-      for (int j = 0; j < m; j++) {
-        Complex &P1 = P[i + j + m], &P2 = P[i + j];
-        Complex t = unit * P1;
-        P1 = P2 - t; P2 = P2 + t;
-        unit = unit * unit_p0;
-      }
-    }
-  }
+
+void multiply(const vector <int> & a, const vector <int> & b,vector<int> &res) {
+	vector <base> fa(a.begin() , a.end()), fb(b.begin(), b.end()) ;
+	size_t n = 1 ;
+	while (n < max (a.size(), b.size ()))  n <<= 1 ;
+	n <<= 1 ;
+	fa.resize(n) ,  fb. resize ( n ) ;
+	fft(fa,false) , fft (fb,false) ;
+	for ( size_t i = 0 ; i < n ; ++ i )
+		fa[i] *= fb[i] ;
+	fft (fa, true) ;
+    res.resize (n);
+    for (size_t i = 0; i < n; ++i)
+        res[i] = (int) (fa[i].real() + 0.5);
 }
-vector<int> mul(const vector<int> &a, const vector<int> &b) {
-  vector<int> ret(max(0, (int) a.size() + (int) b.size() - 1), 0);
-  int len = 1; while (len < (int)ret.size()) len <<= 1;
-  static Complex A[1000000], B[1000000], C[1000000]; // adjust accordingly
-  for (int i = 0; i < len; i++) A[i] = i < (int)a.size() ? a[i] : 0;
-  for (int i = 0; i < len; i++) B[i] = i < (int)b.size() ? b[i] : 0;
-  fft(A, len, 1); fft(B, len, 1);
-  for (int i = 0; i < len; i++) C[i] = A[i] * B[i];
-  fft(C, len, -1); for (int i = 0; i < (int)ret.size(); i++) ret[i] = (long long)((C[i]/Complex(len,0)).real() + 0.5);
-  return ret;
+ 
+vi mul(vi a , vi b, vi res){
+	multiply(a,b,res);
+	return res;
 }
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// NTT
-
-namespace FFT {
-	//  `替代方案: $23068673( = 11 * 2 ^ {21} + 1)$, 原根为 $3$`
-	const int MOD = 998244353;
-	const int PRIMITIVE_ROOT = 3; // `$3 * 2 ^ {18} + 1$`
-	const int MAXB = 1 << 20;
-	// int getMod(int downLimit) { // `或者现场自己找一个MOD`
-	//   for (int c = 3; ; ++c) { int t = (c << 21) | 1;
-	//     if (t >= downLimit && isPrime(t)) return t;
-	// }}
-	inline ll powr(ll a , ll b){
-		ll t =1 ;
-		while(b){
-			if(b&1) t=mul(t,a);	
-			a=mul(a,a);
-			b>>=1;
-		}
-		return t%MOD;
-	}
-	inline ll inv(ll a){ return powr(a,MOD-2); }
-
-	void NTT(int P[], int n, int oper) {
-	  for (int i = 1, j = 0; i < n - 1; i++) {
-		for (int s = n; j ^= s >>= 1, ~j & s;);
-		if (i < j) swap(P[i], P[j]);
-	  }
-	  for (int d = 0; (1 << d) < n; d++) {
-		int m = 1 << d, m2 = m * 2;
-		long long unit_p0 = powr(PRIMITIVE_ROOT, (MOD - 1) / m2);
-		if (oper < 0) unit_p0 = inv(unit_p0);
-		for (int i = 0; i < n; i += m2) {
-		  long long unit = 1;
-		  for (int j = 0; j < m; j++) {
-			int &P1 = P[i + j + m], &P2 = P[i + j];
-			int t = unit * P1 % MOD;
-			P1 = (P2 - t + MOD) % MOD; P2 = (P2 + t) % MOD;
-			unit = unit * unit_p0 % MOD;
-		  }
-		}
-	  }
-	}
-	vector<ll> mul(const vector<ll> &a, const vector<ll> &b) {
-	  vector<ll> ret(max(0, (int) a.size() + (int) b.size() - 1), 0);
-	  static int A[MAXB], B[MAXB], C[MAXB];
-	  int len = 1; while (len < (int)ret.size()) len <<= 1;
-	  for (int i = 0; i < len; i++) A[i] = i < (int)a.size() ? a[i] : 0;
-	  for (int i = 0; i < len; i++) B[i] = i < (int)b.size() ? b[i] : 0;
-	  NTT(A, len, 1); NTT(B, len, 1);
-	  for (int i = 0; i < len; i++) C[i] = (long long) A[i] * B[i] % MOD;
-	  NTT(C, len, -1); for (int i = 0, invv = inv(len); i < (int)ret.size(); i++) ret[i] = (long long) C[i] * invv % MOD;
-	  return ret;
-	}
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
