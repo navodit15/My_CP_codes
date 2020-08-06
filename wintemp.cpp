be4498b07fb2caebcb9bf5e19ec787023cf0bf20
Navoditn <39704421+navodit15@users.noreply.github.com>
2020-08-06 20:49:42 +0530

    Update wintemp.cpp

--- wintemp.cpp
+++ wintemp.cpp
@@ -15,26 +15,36 @@ using namespace std;
 #define fst first
 #define snd second
 using namespace __gnu_pbds;
-using ordered_set = tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>;
+using ordered_set =
+ tree<int, null_type,less<int>,
+ rb_tree_tag,tree_order_statistics_node_update>;
+template<class T>
+using min_heap = priority_queue<T,vector<T>,greater<T>>; 
 template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);}
 template<typename T> T lcm(T a, T b){return(a*(b/gcd(a,b)));}
-typedef long long ll;typedef pair<int, int> pii;typedef vector<int> vi;typedef long double ld;
-template<typename T> void remdup(vector<T>& v) {sort(all(v)); v.erase(unique(all(v)), v.end());}
+typedef long long ll;typedef pair<int, int> pii;
+typedef vector<int> vi;typedef long double ld;
+template<typename T> 
+void remdup(vector<T>& v) {
+	sort(all(v)); v.erase(unique(all(v)), v.end());
+}
+template<typename T> void re(vector<T> &v) {trav(i,v) cin >> i;}
+void unsyncIO() { ios_base::sync_with_stdio(0); cin.tie(0); }
 #ifdef np
 #include "C:\Users\navodit\Desktop\mycodes\pr.h"
 #else
 #define trace(...)
 #endif
-/* TOO WEAK TO SLOW */
+/* ******************* TOO WEAK TOO SLOW ********************* */
+
 #define MOD 1000000007  
 
-void unsyncIO() { ios_base::sync_with_stdio(0); cin.tie(0); }
 
 
 
 
 void solve(){
-
+	
 	
 }
 
@@ -43,6 +53,15 @@ signed main() {
 	int tt =1 ;
 	// cin >> tt;
 	rep(i,0,tt) solve();
+
+
+
+
+
+
+
+
+
 
 
 
