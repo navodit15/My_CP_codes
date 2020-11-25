ad4d22800ade526f09aa9e1de1e602a2c6e32230
Navoditn <39704421+navodit15@users.noreply.github.com>
2020-11-25 20:43:45 +0530

    Update ModInt.cpp

--- Math./ModInt.cpp
+++ Math./ModInt.cpp
@@ -1,49 +1,6 @@
 // ref : https://codeforces.com/blog/entry/63903
 // Verification :CSES
 
-#include <bits/stdc++.h>
-using namespace std;
-#pragma GCC optimize("O3")
-#pragma GCC optimize("unroll-loops") 
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp> 
-//#define int long long
-#define fr(i,a,b) for(int i = a ; i <= b ; ++i)
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
-#define trav(a, x) for(auto& a : x)
-#define all(x) begin(x), end(x)
-#define rall(x) (x).rbegin(), (x).rend()
-#define sz(x) (int)(x).size()
-#define pb push_back
-#define fst first
-#define snd second
-using namespace __gnu_pbds;
-using ordered_set =
- tree<int, null_type,less<int>,
- rb_tree_tag,tree_order_statistics_node_update>;
-//find_by_order(k):returns iterator to kth element starting from 0
-//order_of_key(k):returns count of elements strictly smaller than k
-typedef long long ll;typedef pair<int, int> pii;
-typedef vector<int> vi;typedef long double ld;
-template<class T>
-using min_heap = priority_queue<T,vector<T>,greater<T>>; 
-template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);}
-template<typename T> T lcm(T a, T b){return(a*(b/gcd(a,b)));}
-template<typename T> 
-void remdup(vector<T>& v) {
-	sort(all(v)); v.erase(unique(all(v)), v.end());
-}
-template<typename T> void re(vector<T> &v) {trav(i,v) cin >> i;}
-void unsyncIO() { ios_base::sync_with_stdio(0); cin.tie(0); }
-#ifdef np
-#include "C:\Users\navodit\Desktop\mycodes\pr.h"
-#else
-#define trace(...)
-#endif
-
-#define MOD 1000000007  
-
-
 template<unsigned P> struct ModInt {
   using M = ModInt;
   unsigned v;
@@ -81,6 +38,7 @@ template<unsigned P> struct ModInt {
     M res = 1;
     for (M a = *this; n; a *= a, n >>= 1) if (n & 1) res *= a;
     return res;
+    // mint(a).pow(b);
   }
 };
 using Mint = ModInt<998244353>; // 
@@ -109,36 +67,5 @@ struct Minv {
 Mint ncr(int n, int r) {
   if (r < 0 or r > n) return 0;
   return fact[n] * finv[r] * finv[n - r];
-}
-
-
-void solve(){
-	int n  ,r ; 
-	cin >> n >> r ;
-	cout << ncr (n,r) << endl;
-}
-
-signed main() {
-	unsyncIO();
-	int tt =1 ;
-	//cin >> tt;
-	rep(i,0,tt) solve();
-
-
-
-
-
-
-
-
-
-
-
-
-
-#ifdef np
-	cout <<endl<<endl<< "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
-#endif
-	return 0;
 }
 
