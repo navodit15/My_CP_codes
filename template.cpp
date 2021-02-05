4c544dc64ecfa200be486da3774ff233b678c6bc
Navoditn <39704421+navodit15@users.noreply.github.com>
2021-02-06 02:11:23 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -49,27 +49,7 @@ void unsyncIO() { ios_base::sync_with_stdio(0); cin.tie(0); }
 #endif
 
 
-namespace std {
- 
-template<class Fun>
-class y_combinator_result {
-  Fun fun_;
-public:
-  template<class T>
-  explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
- 
-  template<class ...Args>
-  decltype(auto) operator()(Args &&...args) {
-    return fun_(std::ref(*this), std::forward<Args>(args)...);
-  }
-};
- 
-template<class Fun>
-decltype(auto) y_combinator(Fun &&fun) {
-  return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
-}
- 
-} // namespace std
+
 
 constexpr int MOD = 1000000007;
 constexpr int Nx = 2e5+5; // check 
