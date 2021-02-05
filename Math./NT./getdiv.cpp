0716307866cb5265c16c11869393aaecfb37d00a
Navoditn <39704421+navodit15@users.noreply.github.com>
2021-02-05 17:03:30 +0530

    Create getdiv.cpp

---
+++ Math./NT./getdiv.cpp
@@ -0,0 +1,20 @@
+
+vector<pii> factor(ll x) {
+    vector<pii> ans;
+    for(ll i = 2; i * i <= x; i++)
+        if(x % i == 0) {
+            ans.push_back({i, 1});
+            while((x /= i) % i == 0) ans.back().second++;
+        }
+    if(x != 1) ans.push_back({x, 1});
+    return ans;
+}
+template <class T> vector<T> divisor(T x) {
+    vector<T> ans;
+    for(T i = 1; i * i <= x; i++)
+        if(x % i == 0) {
+            ans.pb(i);
+            if(i * i != x) ans.pb(x / i);
+        }
+    return ans;
+}
