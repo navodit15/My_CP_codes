4f8558ed029c8767122b63a8847630bada7988ec
Navoditn <39704421+navodit15@users.noreply.github.com>
2021-04-04 15:08:12 +0530

    Update lca.cpp

--- graph/lca.cpp
+++ graph/lca.cpp
@@ -25,7 +25,19 @@ int lca(int u, int v) {
 	}
 	return u == v ? u : tab[0][u];
 }
- 
+
+int anc(int v ,int k){ // kth anc of v
+    if(k>=lvl[v]) return 0 ; 
+    int ret = v ;
+    for(int i = MAXLG - 1 ; i>=0 ; i--){
+        if((1ll<<i) <= k) {
+            k-=(1ll<<i);
+            ret = tab[ret][i];
+        }
+    } 
+    return ret ;
+}  
+
 int dist(int u, int v) {
 	return lvl[u] + lvl[v] - 2 * lvl[lca(u, v)];
 }
