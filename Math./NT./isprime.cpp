dbf16de6f4ec2897ce6d096c570db4cdf30de25e
Navoditn <39704421+navodit15@users.noreply.github.com>
2022-01-06 20:59:56 +0530

    Update isprime.cpp

--- Math./NT./isprime.cpp
+++ Math./NT./isprime.cpp
@@ -1,3 +1,29 @@
+vector<pll> factor(ll x) {
+    vector<pll> ans;
+    for(ll i = 2; i * i <= x; i++)
+        if(x % i == 0) {
+            ans.push_back({i, 1});
+            while((x /= i) % i == 0) ans.back().second++;
+        }
+    if(x != 1) ans.push_back({x, 1});
+    return ans;
+}
+template <class T> vector<T> divisor(T x) {
+    vector<T> ans;
+    for(T i = 1; i * i <= x; i++)
+        if(x % i == 0) {
+            ans.pb(i);
+            if(i * i != x) ans.pb(x / i);
+        }
+    return ans;
+}
+template <typename T> void zip(vector<T> &x) {
+    vector<T> y = x;
+    UNIQUE(y);
+    for(int i = 0; i < x.size(); ++i) { x[i] = lb(y, x[i]); }
+}
+
+
 ll modmult(ll x, ll y, ll MOD) {
 	ll a = x, r = 0;
 	while(y) {
