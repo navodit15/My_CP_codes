a2365477103cd4bf84c4467d92362d15c06a4022
Navoditn <39704421+navodit15@users.noreply.github.com>
2022-01-19 17:29:23 +0530

    Update pr.h

--- pr.h
+++ pr.h
@@ -1,5 +1,6 @@
 template<class T> ostream& operator<<(ostream &os, vector<T> V) {os << "[ "; for(auto v : V) os << v << " "; return os << "]";}
 template<class T> ostream& operator<<(ostream &os, set<T> S){os << "{ "; for(auto s:S) os<<s<<" "; return os<<"}";}
+template<class T> ostream& operator<<(ostream &os, multiset<T> MS){os << "{ "; for(auto s:MS) os<<s<<" "; return os<<"}";}
 template<class L, class R> ostream& operator<<(ostream &os, pair<L,R> P) {return os << "(" << P.first << "," << P.second << ")";}
 template<class L, class R> ostream& operator<<(ostream &os, map<L,R> M) {os << "{ "; for(auto m:M) os<<"("<<m.first<<":"<<m.second<<") "; return os<<"}";}
 template<class P, class Q = vector<P>, class R = less<P> > ostream& operator << (ostream& out, priority_queue<P, Q, R> const& M){
